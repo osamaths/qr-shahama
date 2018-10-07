c6aca2c689956720579547b2208fa05d27dc8ef2
osama <osama@mxd3d.com>
2018-10-07 13:26:32 +0300

    change textDetected to false

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      textDetected: true,
+      textDetected: false,
       loading: false
     };
     this.decryptKey = this.decryptKey.bind(this);
