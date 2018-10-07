645db1a73a31036b3241097164785b107a31fb7d
osama <osama@mxd3d.com>
2018-10-07 15:08:14 +0300

    added lineHeight to 50

--- Components/Letter.js
+++ Components/Letter.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-const WORDS = "ما مكني فيه ربي خير";
+const WORDS =
+  "الخطوط متاحة للتحميل لا يعني أنها مجانية حتما، المرجو التحقق من التراخيص عبر صفحة تفاصيل الخط أو من خلال غوغل.";
 // "With your fonts embedded and referenced it’s a simple case of adding them to your React Native styles. Simply add a fontFamily property with your font name:";
 export default class Letter extends Component {
   constructor(props) {
@@ -12,7 +13,7 @@ export default class Letter extends Component {
   render() {
     return (
       <View>
-        <Text style={styles.title}>{"You are My Love"}</Text>
+        <Text style={styles.title}>{"شهد"}</Text>
         <Text style={styles.centerText}>{WORDS}</Text>
       </View>
     );
@@ -24,16 +25,18 @@ const styles = StyleSheet.create({
     fontSize: 30,
     padding: 10,
     color: "peru",
-    fontFamily: "DIVANMAZ"
+    fontFamily: "DIVANMAZ",
+    lineHeight: 50
+
     // fontWeight: "600"
   },
   title: {
-    fontSize: 40,
+    fontSize: 55,
     alignSelf: "center",
     marginTop: 10,
     color: "gold",
-    fontFamily: "cursive",
-    fontWeight: "bold",
+    fontFamily: "DIVANMAZ",
+    // fontWeight: "bold",
     marginBottom: 20
   }
 });
